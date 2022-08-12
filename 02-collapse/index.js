4322102adec05edd792e56fbaaf4b283bca2eb64
turbok <postmaster@turbok.by>
2022-08-12 17:29:53 +0300

    animation in process

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -9,9 +9,26 @@ btn.addEventListener('click', () => {
     if (btnHidden.style.display === 'none') {
         btnHidden.style.display = 'block'
         btnVisible.style.display = 'none'
-        
+        let a= content.animate([
+            // { transform: 'translate3D(0, 0, 0)' },
+            // { transform: 'translate3D(300px, 0, 3000px)' }
+          {top: "100px",
+            heigth: '20px'
+        },
+          {top: 0
+            },
+            {overflow: 'hidden'}
+          ], 1000);
+          setTimeout(()=>{a.pause()}, 900)
+          
     }else {
         btnHidden.style.display ='none' 
         btnVisible.style.display = 'block'
+        const a= content.animate([
+            {opacity: 0 },
+            {opacity: 1}
+          ], 500)
+
+          setTimeout(()=>{a.pause()}, 450)
     }
 })
