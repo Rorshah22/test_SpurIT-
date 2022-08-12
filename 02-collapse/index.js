5124c1e29edf50876cd5569c194857378b70dce1
turbok <postmaster@turbok.by>
2022-08-12 15:41:52 +0300

    feat: add toggle js

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,17 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const btn = document.querySelector('.collapsible__button');
+const btnVisible = document.querySelector('.collapsible__action--visible');
+const btnHidden = document.querySelector('.collapsible__action--hidden');
+const content = document.querySelector('.collapsible__content');
+
+btnHidden.style.display = 'none'
+
+btn.addEventListener('click', () => {
+    if (btnHidden.style.display === 'none') {
+        btnHidden.style.display = 'block'
+        btnVisible.style.display = 'none'
+        
+    }else {
+        btnHidden.style.display ='none' 
+        btnVisible.style.display = 'block'
+    }
+})
