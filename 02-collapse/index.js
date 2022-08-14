66b87be20a3cc835420b02809e25906e9130035c
Searhei Khalimon <hulk145@gmail.com>
2022-08-14 11:08:29 +0300

    task 2 finished

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -3,32 +3,26 @@ const btnVisible = document.querySelector('.collapsible__action--visible');
 const btnHidden = document.querySelector('.collapsible__action--hidden');
 const content = document.querySelector('.collapsible__content');
 
-btnHidden.style.display = 'none'
+const collapse = content.animate(
+  {
+    width: ["1600px", "0px"],
+    height: ["15px", "0"],
+    overflow: "hidden"
+  },
+  { duration: 300, fill: "both", easing: "linear" },
+)
+
+btnHidden.style.display = 'none';
+collapse.pause();
 
 btn.addEventListener('click', () => {
-    if (btnHidden.style.display === 'none') {
-        btnHidden.style.display = 'block'
-        btnVisible.style.display = 'none'
-        let a= content.animate([
-            // { transform: 'translate3D(0, 0, 0)' },
-            // { transform: 'translate3D(300px, 0, 3000px)' }
-          {top: "100px",
-            heigth: '20px'
-        },
-          {top: 0
-            },
-            {overflow: 'hidden'}
-          ], 1000);
-          setTimeout(()=>{a.pause()}, 900)
-          
-    }else {
-        btnHidden.style.display ='none' 
-        btnVisible.style.display = 'block'
-        const a= content.animate([
-            {opacity: 0 },
-            {opacity: 1}
-          ], 500)
-
-          setTimeout(()=>{a.pause()}, 450)
-    }
+  if (collapse.playState === 'paused') {
+    btnHidden.style.display = 'block'
+    btnVisible.style.display = 'none'
+    collapse.play();
+  } else {
+    btnHidden.style.display = 'none'
+    btnVisible.style.display = 'block'
+    collapse.reverse()
+  }
 })
